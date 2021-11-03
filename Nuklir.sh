efe15ea263bd009084029d947975146671d4be55
NymouseX <saibaougokasu@gmail.com>
2021-11-03 13:24:15 +0700

    CodeNuklir

---
+++ Nuklir.sh
@@ -0,0 +1,272 @@
+echo "Memperoses code"  
+sleep 0.1
+clear
+echo "mEmperoses code"  
+sleep 0.1
+clear
+echo "meMperoses code"  
+sleep 0.1
+clear
+echo "memPeroses code" 
+sleep 0.1
+clear
+echo "mempEroses code"  
+sleep 0.1
+clear
+echo "mempeRoses code"  
+sleep 0.1
+clear
+echo "memperOses code" 
+sleep 0.1
+clear
+echo "memperoSes code"  
+sleep 0.1
+clear
+echo "memperosEs code"  
+sleep 0.1
+clear
+echo "memperoseS code"  
+sleep 0.1
+clear
+echo "memperoses Code"  
+sleep 0.1
+clear
+echo "memperoses cOde"  
+sleep 0.1
+clear
+echo "memperoses coDe"  
+sleep 0.1
+clear
+echo "memperoses codE"  
+sleep 0.1
+clear
+echo "Memperoses code" 
+sleep 0.1
+clear
+echo "mEmperoses code" 
+sleep 0.1
+clear
+echo "meMperoses code" 
+sleep 0.1
+clear
+echo "memPeroses code" 
+sleep 0.1
+clear
+echo "mempEroses code" 
+sleep 0.1
+clear
+echo "mempeRoses code" 
+sleep 0.1
+clear
+echo "memperOses code" 
+sleep 0.1
+clear
+echo "memperoSes code" 
+sleep 0.1
+clear
+echo "memperosEs code" 
+sleep 0.1
+clear
+echo "memperoseS code" 
+sleep 0.1
+clear
+echo "memperoses Code" 
+sleep 0.1
+clear
+echo "memperoses cOde" 
+sleep 0.1
+clear
+echo "memperoses coDe" 
+sleep 0.1
+clear
+echo "memperoses codE" 
+sleep 0.1
+clear
+echo "Memperoses code"  
+sleep 0.1
+clear
+echo "mEmperoses code"  
+sleep 0.1
+clear
+echo "meMperoses code"  
+sleep 0.1
+clear
+echo "memPeroses code"  
+sleep 0.1
+clear
+echo "mempEroses code"  
+sleep 0.1
+clear
+echo "mempeRoses code"  
+sleep 0.1
+clear
+echo "memperOses code"  
+sleep 0.1
+clear
+echo "memperoSes code" 
+sleep 0.1
+clear
+echo "memperosEs code" 
+sleep 0.1
+clear
+echo "memperoseS code"  
+sleep 0.1
+clear
+echo "memperoses Code" 
+sleep 0.1
+clear
+echo "memperoses cOde" 
+sleep 0.1
+clear
+echo "memperoses coDe"  
+sleep 0.1
+clear
+echo "memperoses codE" 
+sleep 0.1
+clear
+echo "Memperoses code"  
+sleep 0.1
+clear
+echo "mEmperoses code" 
+sleep 0.1
+clear
+echo "meMperoses code"  
+sleep 0.1
+clear
+echo "memPeroses code" 
+sleep 0.1
+clear
+echo "mempEroses code"  
+sleep 0.1
+clear
+echo "mempeRoses code"  
+sleep 0.1
+clear
+echo "memperOses code" 
+sleep 0.1
+clear
+echo "memperoSes code"  
+sleep 0.1
+clear
+echo "memperosEs code"  
+sleep 0.1
+clear
+echo "memperoseS code"  
+sleep 0.1
+clear
+echo "memperoses Code" 
+sleep 0.1
+clear
+echo "memperoses cOde"  
+sleep 0.1
+clear 
+echo "memperoses coDe" 
+sleep 0.1
+clear
+echo "memperoses codE"  
+sleep 0.1
+clear
+echo "
+░█████╗░░█████╗░██████╗░███████╗███╗░░██╗██╗░░░██╗██╗░░██╗██╗░░░░░██╗██████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗░██║██║░░░██║██║░██╔╝██║░░░░░██║██╔══██╗
+██║░░╚═╝██║░░██║██║░░██║█████╗░░██╔██╗██║██║░░░██║█████═╝░██║░░░░░██║██████╔╝
+██║░░██╗██║░░██║██║░░██║██╔══╝░░██║╚████║██║░░░██║██╔═██╗░██║░░░░░██║██╔══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗██║░╚███║╚██████╔╝██║░╚██╗███████╗██║██║░░██║
+░╚════╝░░╚════╝░╚═════╝░╚══════╝╚═╝░░╚══╝░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝╚═╝░░╚═╝" | lolcat
+echo "***********************NYX*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************NYX*********************************"|lolcat
+echo
+echo "Pilih Katergorinya ;" | lolcat
+echo "1.Full Color" |lolcat
+echo "2.Loli" | lolcat
+echo "3.Sekolah" | lolcat
+echo "4.Sexy" | lolcat
+read -p "|cin.pw}->" CN
+
+if [ $CN = 1 ] || [ $CN = 1 ]
+then
+clear
+echo "
+███████╗██╗░░░██╗██╗░░░░░██╗░░░░░░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░
+██╔════╝██║░░░██║██║░░░░░██║░░░░░██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗
+█████╗░░██║░░░██║██║░░░░░██║░░░░░██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝
+██╔══╝░░██║░░░██║██║░░░░░██║░░░░░██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗
+██║░░░░░╚██████╔╝███████╗███████╗╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║
+╚═╝░░░░░░╚═════╝░╚══════╝╚══════╝░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝" | lolcat
+echo 
+echo "107965 = https://cin.pw/v/107965" | lolcat
+echo "244327 = https://cin.pw/v/244327" | lolcat
+echo "289248 = https://cin.pw/v/289248" | lolcat
+echo "357656 = https://cin.pw/v/357656" | lolcat
+echo "376445 = https://cin.pw/v/376445" | lolcat
+echo "377321 = https://cin.pw/v/377321" | lolcat
+sleep 10
+fi
+
+if [ $CN = 2 ] || [ $CN = 2 ]
+then
+clear
+echo "
+██╗░░░░░░█████╗░██╗░░░░░██╗
+██║░░░░░██╔══██╗██║░░░░░██║
+██║░░░░░██║░░██║██║░░░░░██║
+██║░░░░░██║░░██║██║░░░░░██║
+███████╗╚█████╔╝███████╗██║
+╚══════╝░╚════╝░╚══════╝╚═╝" | lolcat
+echo
+echo "244327 = https://cin.pw/v/244327" | lolcat
+echo "107965 = https://cin.pw/v/107965" | lolcat
+echo "357656 = https://cin.pw/v/357656" | lolcat
+echo "376445 = https://cin.pw/v/376445" | lolcat
+echo "376715 = https://cin.pw/v/376715" | lolcat
+echo "376819 = https://cin.pw/v/376819" | lolcat
+echo
+sleep 10
+fi
+
+if [ $CN = 3 ] || [ $CN = 3 ]
+then
+clear
+echo "
+░██████╗███████╗██╗░░██╗░█████╗░██╗░░░░░░█████╗░██╗░░██╗
+██╔════╝██╔════╝██║░██╔╝██╔══██╗██║░░░░░██╔══██╗██║░░██║
+╚█████╗░█████╗░░█████═╝░██║░░██║██║░░░░░███████║███████║
+░╚═══██╗██╔══╝░░██╔═██╗░██║░░██║██║░░░░░██╔══██║██╔══██║
+██████╔╝███████╗██║░╚██╗╚█████╔╝███████╗██║░░██║██║░░██║
+╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝" | lolcat
+echo
+echo "177013 = https://cin.pw/v/177013" | lolcat
+echo "107965 = https://cin.pw/v/107965" | lolcat
+echo "289248 = https://cin.pw/v/289248" | lolcat
+echo "376445 = https://cin.pw/v/376445" | lolcat
+echo "377321 = https://cin.pw/v/377321" | lolcat
+echo
+sleep 10
+fi
+
+if [ $CN = 4 ] || [ $CN = 4 ]
+then
+clear
+echo "
+░██████╗███████╗██╗░░██╗██╗░░░██╗
+██╔════╝██╔════╝╚██╗██╔╝╚██╗░██╔╝
+╚█████╗░█████╗░░░╚███╔╝░░╚████╔╝░
+░╚═══██╗██╔══╝░░░██╔██╗░░░╚██╔╝░░
+██████╔╝███████╗██╔╝╚██╗░░░██║░░░
+╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░" | lolcat
+echo
+echo "377321 = https://cin.pw/v/377321" | lolcat
+echo "274788 = https://cin.pw/v/274788" | lolcat
+echo "289248 = https://cin.pw/v/289248" | lolcat
+echo "111111 = https://cin.pw/v/111111" | lolcat
+echo "376675 = https://cin.pw/v/376675"  | lolcat
+echo
+sleep 10
+fi
+
+
+
+
